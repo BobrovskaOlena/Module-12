6b31b971a5b52dfe2809e4bcf4fde76b281dfeb4
BobrovskaOlena <lena69114@gmail.com>
2023-03-21 00:04:56 +0200

    4

--- Task1/TimeProgramTests.java
+++ Task1/TimeProgramTests.java
@@ -1,47 +1,40 @@
 package Task1;
 import java.util.concurrent.TimeUnit;
 
-import static java.lang.Thread.*;
-
 public class TimeProgramTests {
         public static void main(String[] args) {
             Thread timeOfWorkThread = new Thread(TimeProgramTests::run);
+            Thread notificationThread = new Thread(TimeProgramTests::run2);
 
-            // Створення потоку для виведення повідомлення про минулих 5 секунд
-            Thread messageThread = new Thread(() -> {
-                while (true) {
-                    try {
-                        TimeUnit.SECONDS.sleep(5);
-                        System.out.println("Минуло 5 секунд");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-            // Запуск потоків
             timeOfWorkThread.start();
-            messageThread.start();
-        }
-
-        // Функція для форматування часу у години, хвилини, секунди
-        private static String formatTime(long millis) {
-            long hours = TimeUnit.MILLISECONDS.toHours(millis);
-            long minutes = TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(hours);
-            long seconds = TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(minutes) - TimeUnit.HOURS.toSeconds(hours);
-            return String.format("%02d:%02d:%02d", hours, minutes, seconds);
-        }
+            notificationThread.start();}
 
     private static void run() {
         long programStart = System.currentTimeMillis();
         while (true) {
             long passedTime = System.currentTimeMillis() - programStart;
-            System.out.println("Passed time: " + formatTime(passedTime));
+            System.out.println("Passed time: " + formatting(passedTime));
             try {
-                sleep(1000);
+                TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
+            private static void run2 () {
+                while (true) {
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                        System.out.println("5 seconds have passed!");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            private static String formatting(long millis){
+                long hours = TimeUnit.MILLISECONDS.toHours(millis);
+                long minutes = TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(hours);
+                long seconds = TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(minutes) - TimeUnit.HOURS.toSeconds(hours);
+                return hours+" hours "+minutes+" minutes "+seconds+" seconds...";
+            }
 }
